6939eb97dd55278e8f9f18e4207116ef6008a364
PazTorresPraderio <cptorres@miuandes.cl>
2019-04-04 21:45:34 -0300

    Final commit

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,15 @@
 class Ticket < ApplicationRecord
   belongs_to :ticket_type
   belongs_to :order
+  before_create :date_cannot_be_in_past
+  before_create :date_cannot_be_in_past_2
 
+  def date_cannot_be_in_past
+  	if self.start_day < Date.today
+  	end
+  end
+  def date_cannot_be_in_past_2
+  	if self.start_day < Date.today
+   end
+  end
 end
